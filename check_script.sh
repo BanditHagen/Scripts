9d525170ff7e631250caccc4d52a3b061021f053
BanditHagen <simonottosson6@gmail.com>
2025-12-09 13:43:36 +0100

    My first script

---
+++ check_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Various checkups on system
+
+echo "=== CPU ==="
+echo
+lscpu
+echo
+echo
+
+echo "=== Disk ==="
+echo
+lsblk
+echo
+echo
+
+echo "=== PCI info ==="
+echo
+lspci
+echo
+echo
+
+echo "=== RAM ==="
+echo
+free -m
+echo
+echo
+
+echo "=== System Info ==="
+echo
+if [[ $EUID -ne 0 ]]; then
+	echo "This section requires sudo privileges. Rerun with sudo ,"
+else
+	sudo dmidecode -t system
+fi
+
+echo
+echo
+echo "=== END ==="
+
+
